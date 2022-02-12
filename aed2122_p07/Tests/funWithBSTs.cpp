982af4247c97c1f557a78fe109de6eaa0eb6c903
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-02-12 11:57:43 +0000

    Ficha 7 completa sem os exercicios extra

--- aed2122_p07/Tests/funWithBSTs.cpp
+++ aed2122_p07/Tests/funWithBSTs.cpp
@@ -55,13 +55,28 @@ int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
 // a) Contando Filmes
 // TODO
 int FunWithBSTs::numberMovies(const vector<pair<string, int>>& reviews) {
-  return 0;
+    set<string> films;
+    for(auto x: reviews)
+        films.insert(x.first);
+  return films.size();
 }
 
 // ..............................
 // b) O filme com mais reviews
 // TODO
 void FunWithBSTs::moreReviews(const vector<pair<string, int>>& reviews, int& m, int& n) {
+    map<string, int> m_counter;
+    m=0;
+    n=0;
+    for (auto x: reviews){
+        if(m_counter.insert(pair<string, int>(x.first,1)).second== false)
+            m_counter[x.first]++;
+        if(m<m_counter[x.first])
+            m=m_counter[x.first];
+    }
+    for (auto x : m_counter){
+        if (x.second == m) n+=1;
+    }
 }
 
 // ..............................
@@ -69,5 +84,22 @@ void FunWithBSTs::moreReviews(const vector<pair<string, int>>& reviews, int& m, 
 // TODO
 vector<string> FunWithBSTs::topMovies(const vector<pair<string, int>>& reviews, double k) {
     vector<string> answer;
+    map<string, double> counter;
+    map<string, double> total;
+
+    for (auto x : reviews){
+        if (!counter.insert(pair<string, double>(x.first, 1.0)).second)
+            counter[x.first]++;
+        if(!total.insert(pair<string, double>(x.first, x.second)).second)
+            total[x.first] = total[x.first] + x.second;
+    }
+
+    for (auto x : counter){
+        total[x.first] = total[x.first] / counter[x.first];
+
+        if (total[x.first] >= k)
+            answer.push_back(x.first);
+    }
+    sort(answer.begin(), answer.end());
     return answer;
 }
--- aed2122_p07/Tests/funWithBSTs.h
+++ aed2122_p07/Tests/funWithBSTs.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
