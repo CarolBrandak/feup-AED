1f639185b1c4ac08deb016c3f3ffec4893e7f27c
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-02-25 08:44:29 +0000

    ficha 1!

--- aed2122_p01/Tests/parque.cpp
+++ aed2122_p01/Tests/parque.cpp
@@ -3,72 +3,70 @@
 
 using namespace std;
 
-ParqueEstacionamento::ParqueEstacionamento(unsigned lot, unsigned nMaxCli):lotacao(lot), numMaximoClientes(nMaxCli){
+ParqueEstacionamento::ParqueEstacionamento(unsigned lot, unsigned nMaxCli):lotacao(lot), numMaximoClientes(nMaxCli) {
     this->clientes={};
     this->vagas=lotacao;
 }
-unsigned ParqueEstacionamento::getNumLugares() const{
+
+unsigned int ParqueEstacionamento::getNumLugares() const {
     return lotacao;
 }
 
-unsigned ParqueEstacionamento::getNumMaximoClientes() const{
+unsigned int ParqueEstacionamento::getNumMaximoClientes() const {
     return numMaximoClientes;
 }
 
-int ParqueEstacionamento::posicaoCliente(const string & nome) const {
-    for (int i = 0; i < clientes.size(); i++) {
-        if (clientes[i].nome == nome)
+int ParqueEstacionamento::posicaoCliente(const string &nome) const {
+    for(int i=0; i<clientes.size(); i++){
+        if(clientes[i].nome==nome)
             return i;
-        else
-            return -1;
-    }
+    }return -1;
 }
 
-bool ParqueEstacionamento::adicionaCliente(const string & nome){
-    if(clientes.size()==numMaximoClientes){
+bool ParqueEstacionamento::adicionaCliente(const string &nome) {
+    if(numMaximoClientes==clientes.size() || posicaoCliente(nome)!=-1)
         return false;
-    }
+
+
     InfoCartao info;
     info.nome=nome;
-    info.presente = false;
+    info.presente=false;
     clientes.push_back(info);
     return true;
-
 }
-bool ParqueEstacionamento::entrar(const string & nome){
-    if(clientes.size()==numMaximoClientes || posicaoCliente(nome)==-1 || clientes[posicaoCliente(nome)].presente==false){
+
+bool ParqueEstacionamento::entrar(const string &nome) {
+    if(vagas==0 || posicaoCliente(nome)==-1 || clientes[posicaoCliente(nome)].presente==true)
         return false;
+    else{
+        clientes[posicaoCliente(nome)].presente=true;
+        vagas--;
+        return true;
     }
-    clientes[posicaoCliente(nome)].presente=true;
-    vagas --;
-    return true;
 }
 
-bool ParqueEstacionamento::retiraCliente(const string & nome){
-    for (vector<InfoCartao>::iterator it = clientes.begin(); it != clientes.end(); it++)
-        if ( (*it).nome == nome ) {
-            if (!(*it).presente) {
-                clientes.erase(it);
-                return true;
-            }
-            else return false;
-        }
-    return false;
+bool ParqueEstacionamento::retiraCliente(const string &nome) {
+    if (posicaoCliente(nome)==-1 )
+        return false;
+    if ( clientes[posicaoCliente(nome)].presente)
+        return false;
+    clientes.erase(clientes.begin() + posicaoCliente(nome));
+    return true;
+
 }
 
 bool ParqueEstacionamento::sair(const string &nome) {
-    if (posicaoCliente(nome) == -1)
+    if (posicaoCliente(nome)==-1 || !clientes[posicaoCliente(nome)].presente)
         return false;
-    if (!clientes[posicaoCliente(nome)].presente)
-        return false;
-    clientes[posicaoCliente(nome)].presente = false;
-    vagas ++;
+    clientes[posicaoCliente(nome)].presente=false;
+    vagas++;
     return true;
 }
 
-unsigned ParqueEstacionamento::getNumLugaresOcupados() const{
+unsigned ParqueEstacionamento::getNumLugaresOcupados() const {
     return lotacao-vagas;
 }
-unsigned ParqueEstacionamento::getNumClientesAtuais() const{
+
+unsigned ParqueEstacionamento::getNumClientesAtuais() const {
     return clientes.size();
 }
