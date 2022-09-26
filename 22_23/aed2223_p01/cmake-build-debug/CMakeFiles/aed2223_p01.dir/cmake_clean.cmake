31cf28e4c8ed758ccc5f0c9e2f90c4ee1f437147
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-09-26 12:24:13 +0100

    add pasta 22_23

---
+++ 22_23/aed2223_p01/cmake-build-debug/CMakeFiles/aed2223_p01.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../Tests/aed2223_p01.exe"
+  "../Tests/aed2223_p01.exe.manifest"
+  "../Tests/aed2223_p01.pdb"
+  "CMakeFiles/aed2223_p01.dir/Tests/funWithCycles.cpp.obj"
+  "CMakeFiles/aed2223_p01.dir/Tests/tests.cpp.obj"
+  "CMakeFiles/aed2223_p01.dir/main.cpp.obj"
+  "libaed2223_p01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed2223_p01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
