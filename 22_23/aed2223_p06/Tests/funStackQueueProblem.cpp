0d58663019577595c4c564d846e5020aaf9ab5c7
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-10-30 22:21:14 +0000

    Ficha 6

--- 22_23/aed2223_p06/Tests/funStackQueueProblem.cpp
+++ 22_23/aed2223_p06/Tests/funStackQueueProblem.cpp
@@ -8,15 +8,26 @@ FunStackQueueProblem::FunStackQueueProblem() {}
 
 // TODO
 vector<string> FunStackQueueProblem::binaryNumbers(int n) {
-    vector<string> res;
+    vector<string> res={};
+    //queue< string> q;
+    //q.push("1");
+
+    for (int i = 1; i <=n ; i++) {
+        int t=0;
+        t=i%2;
+        i=i/2;
+        //q.push(to_string(i));
+        //string s1 = q.front();
+        res.push_back(to_string(t));
+    }
     return res;
 }
-
 
 //TODO
 vector<int> FunStackQueueProblem::calculateSpan(vector<int> prices)
 {
     vector<int> res;
+
     return res;
 }
 
