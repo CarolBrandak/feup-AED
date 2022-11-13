86e08c7f46bb55a5de6757db724f1537b645929d
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-11-13 10:29:17 +0000

    Ficha 4 done

--- 22_23/aed2223_p04/Tests/product.cpp
+++ 22_23/aed2223_p04/Tests/product.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "product.h"
 
 Product::Product(string i, float p, float w): id(i), price(p), weight(w) {}
@@ -21,27 +20,3 @@ bool Product::operator < (const Product &p) const{
     }
     return this->getPrice()<p.getPrice();
 }
-=======
-#include "product.h"
-
-Product::Product(string i, float p, float w): id(i), price(p), weight(w) {}
-
-string Product::getId() const {
-    return id;
-}
-
-float Product::getPrice() const {
-    return price;
-}
-
-float Product::getWeight() const {
-    return weight;
-}
-
-bool Product::operator < (const Product &p) const{
-    if(this->getPrice()==p.getPrice()){
-        return this->getWeight()<p.getWeight();
-    }
-    return this->getPrice()<p.getPrice();
-}
->>>>>>> 15d654b9bf7aaeb31a544467ffce6ac33cb65a9b
--- 22_23/aed2223_p04/Tests/product.h
+++ 22_23/aed2223_p04/Tests/product.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
@@ -19,25 +18,3 @@ public:
     bool operator<(const Product &p) const;
 };
 #endif
-=======
-#ifndef PRODUCT_H
-#define PRODUCT_H
-
-#include <string>
-#include <system_error>
-
-using namespace std;
-
-class Product : public error_code {
-    string id;
-    float price;
-    float weight;
-public:
-    Product(string i, float p, float w);
-    string getId() const;
-    float getPrice() const;
-    float getWeight() const;
-    bool operator<(const Product &p) const;
-};
-#endif
->>>>>>> 15d654b9bf7aaeb31a544467ffce6ac33cb65a9b
